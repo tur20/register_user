575d443a7cffc5a1d9abbb54a544191847ae4ee9
Tetiana Usynska <tanyausynska@gmail.com1>
2023-03-10 18:20:05 +0200

    some plugins added

--- src/test/java/com/task/CukesRunner.java
+++ src/test/java/com/task/CukesRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html",
+        plugin = { "pretty",
+                "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/task/step_definitions",
